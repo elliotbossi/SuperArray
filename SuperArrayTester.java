ad73e9b0de2b6c42e5b51282bf8d1925bb8bf6d2
Elliot <ebossi10@stuy.edu>
2020-11-04 18:35:32 -0500

    added tester

---
+++ SuperArrayTester.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+public class SuperArrayTester {
+
+  public static void main(String[] args) {
+    SuperArray words = new SuperArray();
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    System.out.println(words.isEmpty());
+    for(int i = 0; i < words.size(); i++){
+      System.out.println(words.get(i) );
+    }
+    words.clear();
+    System.out.println(words.isEmpty());
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    System.out.println(words.toString());
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    words.add("kani");
+    System.out.println(words.toString());
+    words.add(3,"test");
+    System.out.println(words.toString());
+    System.out.println(words.indexOf("test"));
+    words.remove(3);
+    System.out.println(words.toString());
+    System.out.println(words.indexOf("test"));
+    System.out.println(Arrays.toString(words.toArray()));
+
+
+  }
+}
