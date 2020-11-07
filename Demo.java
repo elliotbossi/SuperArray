e4ff2aa563fd921ac27bcba0177e555777a70a6c
Elliot <ebossi10@stuy.edu>
2020-11-06 19:07:26 -0500

    2nd method worked

--- Demo.java
+++ Demo.java
@@ -8,6 +8,19 @@ public class Demo{
       i = i - 1;
     }
   }
+
+  public static SuperArray findOverlap(SuperArray a, SuperArray b){
+    SuperArray result = new SuperArray();
+    int i = 0;
+    while (i<a.size()){
+      if ((b.contains(a.get(i))) && (a.contains(a.get(i)))){
+        result.add(a.get(i));
+      }
+      i = i + 1;
+    }
+    removeDuplicates(result);
+    return result;
+  }
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -17,5 +30,21 @@ public class Demo{
 
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+    SuperArray a = new SuperArray();
+    SuperArray b = new SuperArray();
+    a.add("9");
+    a.add("1");
+    a.add("2");
+    a.add("2");
+    a.add("3");
+    a.add("4");
+
+    b.add("0");
+    b.add("4");
+    b.add("2");
+    b.add("2");
+    b.add("9");
+    System.out.println(findOverlap(a,b));
+    }
 }
