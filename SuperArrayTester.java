9df75ed227839a56bac2681bc882cce42032c14d
Elliot <ebossi10@stuy.edu>
2020-11-11 21:31:36 -0500

    personal and other tester both worked

--- SuperArrayTester.java
+++ SuperArrayTester.java
@@ -8,7 +8,7 @@ public class SuperArrayTester {
     words.add("ebi");
     System.out.println(words.isEmpty());
     for(int i = 0; i < words.size(); i++){
-      System.out.println(words.get(i) );
+      System.out.println(words.get(i));
     }
     words.clear();
     System.out.println(words.isEmpty());
@@ -31,7 +31,43 @@ public class SuperArrayTester {
     System.out.println(words.toString());
     System.out.println(words.indexOf("test"));
     System.out.println(Arrays.toString(words.toArray()));
+        try {
+            SuperArray z;
+            z = new SuperArray(-1);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("cannot be negative");
+        }
+
+        try {
+            words.get(-1);
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("cannot be out of range");
+        }
+
+        try {
+            words.set(-1,"a");
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("cannot be out of range");
+        }
+
+        try {
+            words.add(-1,"a");
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("cannot be out of range");
+        }
+
+        try {
+            words.remove(-1);
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("cannot be out of range");
+        }
 
 
+
+    }
   }
-}
